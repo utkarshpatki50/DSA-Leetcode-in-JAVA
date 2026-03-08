3ba96428258046c6c6d38ecf8146508ca4019fe2
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-03-09 01:33:27 +0530

    Time: 1 ms (99.8%), Space: 48 MB (34.9%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[index++] = num;
+            }
+        }
+
+        while (index < n) {
+            nums[index++] = 0;
+        }
+    }
+}
