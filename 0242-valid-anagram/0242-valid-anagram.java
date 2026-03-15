fbffceef249aa382363cf522d78e1cab8ffd9220
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-03-16 02:19:02 +0530

    Time: 4 ms (81.49%), Space: 43.6 MB (99.79%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        if (m != n) {
+            return false;
+        }
+
+        int[] arr = new int[26];
+        for (int i = 0; i < m; i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
