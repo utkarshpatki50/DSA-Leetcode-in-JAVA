35613171ae6aca27c99570ba72993498ef8e9b79
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-03-17 02:01:18 +0530

    Time: 0 ms (100%), Space: 42.9 MB (81.39%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,12 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.trim();
+        int n = s.length();
         int count = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == ' ') {
-                break;
-            } else {
+
+        if (n == 0)
+            return 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
                 count++;
+            } else {
+                if (count > 0)
+                    return count;
             }
         }
         return count;
