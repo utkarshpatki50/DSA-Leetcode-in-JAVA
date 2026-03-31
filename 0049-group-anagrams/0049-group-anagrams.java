d0390bec87d60bed9d3f02aa4acb932363cad5a7
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-03-31 13:40:27 +0530

    Time: 20 ms (14.38%), Space: 50.1 MB (27.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,18 +1,34 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+
         HashMap<String, List<String>> mp = new HashMap<>();
         for (String word : strs) {
-            char[] chars = word.toCharArray();
-            Arrays.sort(chars);
+            int[] freq = new int[26];
 
-            String key = new String(chars);
+            for (char c : word.toCharArray()) {
+                freq[c - 'a']++;
+            }
 
+            StringBuilder keyBuilder = new StringBuilder();
+            for (int count : freq) {
+                keyBuilder.append(count).append("#");
+            }
+
+            String key = keyBuilder.toString();
             if (!mp.containsKey(key)) {
                 mp.put(key, new ArrayList<>());
             }
 
             mp.get(key).add(word);
+
         }
         return new ArrayList<>(mp.values());
     }
 }
+
+//Approach 2 Optimized
+// for (String word : strs) {
+// char[] chars = word.toCharArray();
+// Arrays.sort(chars);
+
+// String key = new String(chars);
