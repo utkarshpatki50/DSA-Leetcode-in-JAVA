17a057db4f72ac4e8508b99760b0217451ed509d
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-04-25 14:15:28 +0530

    Time: 2 ms (36.69%), Space: 43.2 MB (15.18%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,33 +1,39 @@
 class Solution {
-
     public String decodeString(String s) {
-        int n = s.length();
-        Stack<Integer> countStack = new Stack<>();
-        Stack<String> strStack = new Stack<>();
+        int n= s.length();
+        Stack<Integer> countStack= new Stack<>();
+        Stack<String> strStack= new Stack<>();
 
-        String currString = "";
-        int num = 0;
+        String currString="";
+        int num=0;
 
-        for (char ch : s.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
-            } else if (ch == '[') {
+        for(char ch: s.toCharArray()){
+            if(Character.isDigit(ch)){
+                num= num*10 + (ch-'0');
+            }
+
+            else if(ch== '['){
                 countStack.push(num);
                 strStack.push(currString);
 
-                num = 0;
-                currString = "";
-            } else if (ch == ']') {
-                int repeat = countStack.pop();
-                String prevString = strStack.pop();
+                num=0;
+                currString="";
 
-                StringBuilder temp = new StringBuilder(prevString);
-                for (int i = 0; i < repeat; i++) {
+            }
+
+            else if(ch== ']'){
+                int repeat= countStack.pop();
+                String prevString= strStack.pop();
+
+                StringBuilder temp= new StringBuilder(prevString);
+                for(int i=0; i<repeat; i++){
                     temp.append(currString);
                 }
 
-                currString = temp.toString();
-            } else {
+                currString= temp.toString();
+            }
+
+            else{
                 currString += ch;
             }
         }
