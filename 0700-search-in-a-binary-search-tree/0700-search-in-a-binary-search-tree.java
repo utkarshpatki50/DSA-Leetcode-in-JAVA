6a84637a2bb3fad90fd38c9c350bd98a268bde47
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-08-19 00:49:38 +0530

    Time: 0 ms (100%), Space: 47.2 MB (16.44%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -15,14 +15,15 @@
  */
 class Solution {
 
-    //recursive
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null || root.val == val)
-            return root;
+        if (root == null) return null;
 
-        if (root.val > val)
+        if (root.val == val) return root;
+
+        if (root.val > val) {
             return searchBST(root.left, val);
-        else
-            return searchBST(root.right, val);
+        }
+
+        return searchBST(root.right, val);
     }
 }
