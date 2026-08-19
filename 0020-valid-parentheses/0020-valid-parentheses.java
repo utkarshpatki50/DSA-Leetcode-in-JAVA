0c30e76d3683d7490fa84f4fe729a5568072b805
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-08-19 22:59:29 +0530

    Time: 3 ms (86.09%), Space: 43.3 MB (58.05%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,25 +1,26 @@
 class Solution {
+
     public boolean isValid(String s) {
         int n = s.length();
         Stack<Character> st = new Stack<>();
 
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
+
             if (ch == '(' || ch == '[' || ch == '{') {
                 st.push(ch);
             } else {
-                if (st.empty()) {
+                if (st.isEmpty()) {
                     return false;
                 }
-                char top = st.pop();
 
-                if (top == '(' && ch != ')'
-                        || top == '[' && ch != ']'
-                        || top == '{' && ch != '}') {
+                char top = st.pop();
+                if (top == '(' && ch != ')' || top == '[' && ch != ']' || top == '{' && ch != '}') {
                     return false;
                 }
             }
         }
+
         return st.isEmpty();
     }
 }
