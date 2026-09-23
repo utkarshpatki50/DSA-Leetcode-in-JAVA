cfdbcd6100bac27e65bf10c8439a22c553652a5c
Utkarsh Patki <utkarshpatki50@gmail.com>
2026-09-24 02:04:04 +0530

    Time: 4 ms (83.73%), Space: 44.6 MB (59.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,22 @@
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        int m = s.length();
-        int n = t.length();
 
-        if (m != n) {
+    public boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+
+        if (n != m) {
             return false;
         }
 
-        int[] arr = new int[26];
-        for (int i = 0; i < m; i++) {
-            arr[s.charAt(i) - 'a']++;
-            arr[t.charAt(i) - 'a']--;
+        int[] freq = new int[26];
+        for (int i = 0; i < n; i++) {
+            freq[s.charAt(i) - 'a']++;
+
+            freq[t.charAt(i) - 'a']--;
         }
 
         for (int i = 0; i < 26; i++) {
-            if (arr[i] != 0) {
+            if (freq[i] != 0) {
                 return false;
             }
         }
